55ae9932301f515b0f51d820fddd09bcdf9adc40
Siddharth Singh <siddhartsingh2002@gmail.com>
2022-03-14 23:02:57 +0530

    Added NoteDatabase and NoteVM

---
+++ app/src/main/java/com/siddydevelops/noteskotlin/NoteViewModel.kt
@@ -0,0 +1,32 @@
+package com.siddydevelops.noteskotlin
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class NoteViewModel(application: Application): AndroidViewModel(application) {
+    val allNotes: LiveData<List<Note>>
+    val repository: NoteRepository
+
+    init {
+        val dao = NoteDatabase.getDatabase(application).getNotesDao()
+        repository = NoteRepository(dao)
+        allNotes = repository.allNotes
+    }
+
+    fun deleteNote(note: Note) = viewModelScope.launch(Dispatchers.IO) {
+        repository.delete(note)
+    }
+
+    fun updateNote(note: Note) = viewModelScope.launch(Dispatchers.IO) {
+        repository.update(note)
+    }
+
+    fun addNote(note: Note) = viewModelScope.launch(Dispatchers.IO) {
+        repository.insert(note)
+    }
+
+}
