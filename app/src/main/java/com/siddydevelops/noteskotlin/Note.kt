07bcffe7acb767a595b6bc2d8df08a7255d7cdcc
Siddharth Singh <siddhartsingh2002@gmail.com>
2022-03-14 22:45:10 +0530

    Added NOTE Entity and Model

---
+++ app/src/main/java/com/siddydevelops/noteskotlin/Note.kt
@@ -0,0 +1,16 @@
+package com.siddydevelops.noteskotlin
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "notesTable")
+class Note (
+    @ColumnInfo(name="title")val noteTitle: String,
+    @ColumnInfo(name = "description")val noteDescription: String,
+    @ColumnInfo(name = "timestamp")val timestamp: String
+    ){
+    @PrimaryKey(autoGenerate = true)
+    var id = 0
+
+}
---
+++ app/src/main/java/com/siddydevelops/noteskotlin/NotesDao.kt
@@ -0,0 +1,19 @@
+package com.siddydevelops.noteskotlin
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface NotesDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(note: Note)
+
+    @Update
+    suspend fun update(note: Note)
+
+    @Delete
+    suspend fun delete(note: Note)
+
+    @Query("Select * from notesTable order by id ASC")
+    fun getAllNotes(): LiveData<List<Note>>
+}
