7470991c00fc10d849fd8a3c9f2c6cdd14f888c4
Siddharth Singh <siddhartsingh2002@gmail.com>
2022-03-14 23:17:16 +0530

    Added init vars

--- app/src/main/java/com/siddydevelops/noteskotlin/MainActivity.kt
+++ app/src/main/java/com/siddydevelops/noteskotlin/MainActivity.kt
@@ -2,12 +2,20 @@ package com.siddydevelops.noteskotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var notesRV: RecyclerView
+    lateinit var addFAB: FloatingActionButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        notesRV = findViewById(R.id.idRVNotes)
+        addFAB = findViewById(R.id.idFABAddNote)
 
 
     }
