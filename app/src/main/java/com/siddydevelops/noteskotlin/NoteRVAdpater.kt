2dd8e3eab612efc0b1acfbac66a06a76c6a02aed
Siddharth Singh <siddhartsingh2002@gmail.com>
2022-03-15 08:10:54 +0530

    Added Adapter

---
+++ app/src/main/java/com/siddydevelops/noteskotlin/NoteRVAdpater.kt
@@ -0,0 +1,63 @@
+package com.siddydevelops.noteskotlin
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class NoteRVAdpater(
+    val context: Context,
+    val noteClickInterface: NoteClickInterface,
+    val noteClickDeleteInterface: NoteClickDeleteInterface
+    ) : RecyclerView.Adapter<NoteRVAdpater.ViewHolder>() {
+
+    private val allNotes = ArrayList<Note>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.note_rv_item,parent,false)
+        return ViewHolder(view)
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.noteTV.text = allNotes[position].noteTitle
+        holder.noteTV.text = "Last Updated: " + allNotes[position].timestamp
+
+        holder.deleteIV.setOnClickListener{
+            noteClickDeleteInterface.onDeleteIconClick(allNotes[position])
+        }
+
+        holder.itemView.setOnClickListener {
+            noteClickInterface.onNoteClick(allNotes[position])
+        }
+
+    }
+
+    override fun getItemCount(): Int {
+        return allNotes.size
+    }
+
+    fun updateList(newList: List<Note>) {
+        allNotes.clear()
+        allNotes.addAll(newList)
+        notifyDataSetChanged()
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val noteTV = itemView.findViewById<TextView>(R.id.idTVNoteTitle)
+        val timeTV = itemView.findViewById<TextView>(R.id.idTVTimeStamp)
+        val deleteIV = itemView.findViewById<ImageView>(R.id.idIVDelete)
+    }
+}
+
+interface NoteClickDeleteInterface {
+    fun onDeleteIconClick(note: Note)
+}
+
+interface NoteClickInterface {
+    fun onNoteClick(note: Note)
+}
